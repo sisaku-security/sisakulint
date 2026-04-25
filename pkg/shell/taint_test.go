312d367980ee04a773b2df269e369f15feb23e76
ultra-supara <atsu120225@gmail.com>
2026-04-25 16:20:45 +0900

    test(shell): #446 forvar コピーを削除 (Go 1.22+)

--- pkg/shell/taint_test.go
+++ pkg/shell/taint_test.go
@@ -93,7 +93,6 @@ func TestWalkAssignments(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			file := parseScript(t, tc.script)
